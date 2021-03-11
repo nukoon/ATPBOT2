0e30c6c1cea77da48fc3d0c2ec1a268d6e857ce7
atpkeng <kengljr>
2021-03-11 15:53:58 +0700

    delete

--- ros_ws/build/mybot_ws/src/mybot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atpkeng/ros_ws/src/mybot_ws/src/mybot_description
-# Build directory: /home/atpkeng/ros_ws/build/mybot_ws/src/mybot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
