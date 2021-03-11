8002872edbccfde1f021a10295e02c08f8320c03
atpkeng <kengljr>
2021-03-11 15:11:18 +0700

    remove

--- ros_ws/build/turtlebot3_simulations/turtlebot3_fake/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atpkeng/ros_ws/src/turtlebot3_simulations/turtlebot3_fake
-# Build directory: /home/atpkeng/ros_ws/build/turtlebot3_simulations/turtlebot3_fake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
