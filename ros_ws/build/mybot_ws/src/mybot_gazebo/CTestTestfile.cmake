a0b93ce7dc718becef598475dc01320a13aae2da
atpkeng <kengljr>
2021-03-11 15:07:13 +0700

    remove wrong folder

--- ros_ws/build/mybot_ws/src/mybot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atpkeng/ros_ws/src/mybot_ws/src/mybot_gazebo
-# Build directory: /home/atpkeng/ros_ws/build/mybot_ws/src/mybot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
